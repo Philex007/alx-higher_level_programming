5e2ae9cd59a04be07a4312b0bf9eb39989d6f1ec
Philex007 <philemon.tamrat@gmail.com>
2024-01-10 09:29:49 -0800

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+3-print_reversed_list_integer.py
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
